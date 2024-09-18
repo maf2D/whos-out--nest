bd43f2b5f7e9c07bc4b122da5fc92061a18bf3cf
Denys Maftulyak <denysmaftulyak@Denyss-MacBook-Pro.local>
2024-09-18 15:00:00 +0300

    add check log for heroku

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
+console.log(process.env);
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
